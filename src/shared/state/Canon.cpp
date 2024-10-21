3fa9387dd2c7a6c2ca03ef904b098bcd364b1d46
trias <trias.nathan0@gmail.com>
2024-10-21 16:32:40 +0200

    Canon, Gold, Food

---
+++ src/shared/state/Canon.cpp
@@ -0,0 +1,17 @@
+//
+// Created by trias on 21/10/24.
+//
+#include "Canon.h"
+
+namespace state {
+
+
+    Canon::~Canon() {
+
+    }
+
+    std::string Canon::getType() {
+    return "Canon";
+    }
+}
+
---
+++ src/shared/state/Gold.cpp
@@ -0,0 +1,16 @@
+//
+// Created by trias on 21/10/24.
+//
+#include "Gold.h"
+
+namespace state {
+
+
+    Gold::~Gold() {
+
+    }
+
+    std::string Gold::getType() {
+        return "Gold";
+    }
+}
