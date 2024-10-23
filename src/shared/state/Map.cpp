bca7ad46b03ce4db56accc3cac82cc9ef7980474
AscentLe <35le.mathieu@gmail.com>
2024-10-23 12:00:40 +0200

    Test constructor for Map

--- src/shared/state/Map.cpp
+++ src/shared/state/Map.cpp
@@ -2,18 +2,28 @@
 
 namespace state {
 
-void Map::setSize(int size)
-{
+void Map::setSize(int size){
     size = size;
 }
 
-int Map::getSize() const
-{
+int Map::getSize() const{
     return size;
 }
 
-Map::~Map()
-{
+Map::~Map(){
 }
 
+Map::Map(int size){
+    listOfTiles[0] = Tile(0, 0, false, 0); 
+    listOfTiles[1] = Tile(2, 0, false, 0); 
+    listOfTiles[2] = Tile(3, 0, false, 0); 
+    listOfTiles[3] = Tile(2, 0, false, 0);
+    listOfTiles[4] = Tile(0, 0, false, 0); 
+    listOfTiles[5] = Tile(0, 2, false, 0);
+    listOfTiles[6] = Tile(0, 3, false, 0); 
+    listOfTiles[7] = Tile(1, 0, false, 0);
+    listOfTiles[8] = Tile(4, 0, false, 0); 
+    listOfTiles[9] = Tile(0, 0, false, 0); 
+    setSize(10);
+}
 }
