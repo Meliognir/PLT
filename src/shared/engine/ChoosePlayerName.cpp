ccd513ab1a5a6713d618b06b788fa19c4aa4925d
trias <trias.nathan0@gmail.com>
2024-12-11 10:56:55 +0100

    Choose

--- src/shared/engine/ChoosePlayerName.cpp
+++ src/shared/engine/ChoosePlayerName.cpp
@@ -2,3 +2,8 @@
 
 engine::ChoosePlayerName::ChoosePlayerName(int player, std::string name) : playerIndex(player), name(name){
 }
+
+void const engine::ChoosePlayerName::launchCommand (state::Game * GamePtr) {
+    state::Player* player = GamePtr->getPlayerList().at(playerIndex);
+    player->setName(name);
+}
