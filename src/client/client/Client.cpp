70080b43008a43dd914cdda75b7b7c507b96f760
trias <trias.nathan0@gmail.com>
2024-12-16 16:15:53 +0100

    Client

--- src/client/client/Client.cpp
+++ src/client/client/Client.cpp
@@ -33,228 +33,260 @@
 #define FOOD 2
 
 
-namespace client{
+namespace client {
+    Client::Client()
+    {
 
-Client::Client()
-{
+        //-----------------------------
+        // instantiates a new Game context and runs concrete states "GameConfig" then "Playing" functions
+        //-----------------------------
+        Renderer renderer;
+        gameState = new state::GameConfigState();
+        gameEngine = new engine::GameEngine(gameState);
+        InputHandler inputHandler;
 
-//-----------------------------
-// instantiates a new Game context and runs concrete states "GameConfig" then "Playing" functions
-//-----------------------------
-    Renderer renderer;
-    gameState = new state::GameConfigState();
-    gameEngine = new engine::GameEngine(gameState);
-    InputHandler inputHandler;
+    }
 
-}
+    int Client::launch(){
 
-int Client::launch(){
+        //renderer-> show the correct window
+        int exiting = 0;
+        int playingMode = 1;
+        std::string modeInput;
+        while(!exiting){
+            //cliquer aux bons endroits pour choisir le mode de jeu souhaité
+            std::cout << "Select a Game Mode" << std::endl;
+            std::cin >> modeInput;
 
-    //renderer-> show the correct window
-    int exiting = 0;
-    int playingMode = 1;
-    std::string modeInput;
-    while(!exiting){
-        //cliquer aux bons endroits pour choisir le mode de jeu souhaité
-        std::cout << "Select a Game Mode" << std::endl;
-        std::cin >> modeInput;
-
-        //switch (playingMode)
-        //{
-        //case /* constant-expression */:
+            //switch (playingMode)
+            //{
+            //case /* constant-expression */:
             /* code */
             /*break;
-        
+
         default:
             break;
         }*/
 
-        //case local :
-        runLocalGame();
-        exiting = 1;     
+            //case local :
+            runLocalGame();
+            exiting = 1;
+        }
+
+        return 0;
+
+
     }
 
-    return 0;
+    int Client::runLocalGame(){
+
+        state::Game *gameInstance = gameEngine->game;
+
+        // Old method :
+        // gameEngine->steps();
 
 
-}
-
-int Client::runLocalGame(){
-
-    state::Game *gameInstance = gameEngine->game;
-
-    // Old method :
-    // gameEngine->steps();
+        // New method :
 
 
-    // New method : 
-
-
-    // case where gameState is GAME_CONFIG_STATE:
-    std::cout << "Client now entering GAME_CONFIG_STATE\r\n" << std::endl;
+        // case where gameState is GAME_CONFIG_STATE:
+        std::cout << "Client now entering GAME_CONFIG_STATE\r\n" << std::endl;
         //do command truc
         gameConfigInit();
         gameInstance->request();
 
-    int endloop = 0;
-    std::string waitConfirm;
-    std::cout << "Client now entering the game loop\r\n" << std::endl;
+        int endloop = 0;
+        std::string waitConfirm;
+        std::cout << "Client now entering the game loop\r\n" << std::endl;
 
-    while (!endloop){
+        while (!endloop){
 
-        switch (gameState->getStateId()/*test1*/){
+            switch (gameState->getStateId()/*test1*/){
 
 
-            case GAME_CONFIG_STATE:
-                std::cout << "Client now entering GAME_CONFIG_STATE\r\n" << std::endl;
+                case GAME_CONFIG_STATE:
+                    std::cout << "Client now entering GAME_CONFIG_STATE\r\n" << std::endl;
                 //do command truc
                 gameInstance->request();
-            break;
+                break;
 
 
-            case CAPTAIN_DICE_STATE:
-                std::cout << "Client now entering CAPTAIN_DICE_STATE\r\n" << std::endl;
+                case CAPTAIN_DICE_STATE:
+                    std::cout << "Client now entering CAPTAIN_DICE_STATE\r\n" << std::endl;
                 //do command truc
                 gameInstance->request();
-            break;
+                break;
 
 
-            case CARD_CHOICE_STATE:
-                std::cout << "Client now entering CARD_CHOICE_STATE\r\n" << std::endl;
+                case CARD_CHOICE_STATE:
+                    std::cout << "Client now entering CARD_CHOICE_STATE\r\n" << std::endl;
                 //do command truc
                 gameInstance->request();
-            break;
+                break;
 
 
-            case CARD_ACTION_STATE:
-                std::cout << "Client now entering CARD_ACTION_STATE\r\n" << std::endl;
+                case CARD_ACTION_STATE:
+                    std::cout << "Client now entering CARD_ACTION_STATE\r\n" << std::endl;
                 //do command truc
                 gameInstance->request();
-            break;
+                break;
 
 
-            case RESOURCE_HANDLING_STATE:
-                std::cout << "Client now entering RESOURCE_HANDLING_STATE\r\n" << std::endl;
+                case RESOURCE_HANDLING_STATE:
+                    std::cout << "Client now entering RESOURCE_HANDLING_STATE\r\n" << std::endl;
                 //do command truc
                 gameInstance->request();
-            break;
+                break;
 
 
-            case OPPONENT_CHOICE_STATE:
-                std::cout << "Client now entering OPPONENT_CHOICE_STATE\r\n" << std::endl;
+                case OPPONENT_CHOICE_STATE:
+                    std::cout << "Client now entering OPPONENT_CHOICE_STATE\r\n" << std::endl;
                 //do command truc
                 gameInstance->request();
-            break;
+                break;
 
 
-            case COMBAT_ATTACKING_STATE:
-                std::cout << "Client now entering COMBAT_ATTACKING_STATE\r\n" << std::endl;
+                case COMBAT_ATTACKING_STATE:
+                    std::cout << "Client now entering COMBAT_ATTACKING_STATE\r\n" << std::endl;
                 //do command truc
                 gameInstance->request();
-            break;
+                break;
 
 
-            case COMBAT_DEFENDING_STATE:
-                std::cout << "Client now entering COMBAT_DEFENDING_STATE\r\n" << std::endl;
+                case COMBAT_DEFENDING_STATE:
+                    std::cout << "Client now entering COMBAT_DEFENDING_STATE\r\n" << std::endl;
                 //do command truc
                 gameInstance->request();
-            break;
+                break;
 
 
-            case GAME_OVER_STATE:
-                std::cout << "Client now entering GAME_OVER_STATE\r\n" << std::endl;
+                case GAME_OVER_STATE:
+                    std::cout << "Client now entering GAME_OVER_STATE\r\n" << std::endl;
                 //do command truc
                 gameInstance->request();
                 endloop = 1;
-            break;
+                break;
+
+            }
+
+            std::cout << "Souhaitez vous continuer le jeu ?" << std::endl;
+            std::cin >> waitConfirm;
+            if (waitConfirm[0] == 'n'){
+                endloop = 1;
+            }
 
         }
+        return 0;
+    }
 
-        std::cout << "Souhaitez vous continuer le jeu ?" << std::endl;
-        std::cin >> waitConfirm;
-        if (waitConfirm[0] == 'n'){
-            endloop = 1;
+
+    int Client::gameConfigInit(){
+
+        state::Game *gameInstance = gameEngine->game;
+
+        int playerNumber = inputHandler.getNumberofPlayers();
+        std::cout <<"Number of players set to: " << std::to_string(playerNumber)<< std::endl;
+        engine::ChooseNbOfPlayers* chooseNbOfPlayers = new engine::ChooseNbOfPlayers(playerNumber);
+        chooseNbOfPlayers->launchCommand(gameInstance);
+        delete chooseNbOfPlayers;
+
+        for(int playerIndex = 0; playerIndex < playerNumber; playerIndex++){
+            std::string playerName = inputHandler.getPlayerName(playerIndex);
+            engine::ChoosePlayerName* choosePlayerName = new engine::ChoosePlayerName(playerIndex, playerName);
+            choosePlayerName->launchCommand(gameInstance);
+            delete choosePlayerName;
         }
 
+        int mapSize = inputHandler.getMapSize();
+        std::cout <<"MapSize set to: " << std::to_string(mapSize)<< std::endl;
+        engine::ChooseMapSize* chooseMapSize = new engine::ChooseMapSize(mapSize);
+        chooseMapSize->launchCommand(gameInstance);
+        delete chooseMapSize;
+
+
+        for(int playerIndex=0; playerIndex < playerNumber; playerIndex++){
+            inputHandler.selectUserBoatHold(6);
+            engine::AddToBoathold* Resource1 = new engine::AddToBoathold(playerIndex,1, 3, "GOLD");
+            Resource1->launchCommand(gameInstance);
+            delete Resource1;
+        }
+
+        printf("Affichage de la map :\n");
+        std::cout<<"Tuile N°0 : Ceci est la tuile de départ, il y a rien à payer ici." << std::endl;
+        for (int i = 1; i < mapSize; i++) {
+            std::cout<< "Tuile N°" << i << " contient : ";
+            if(gameInstance->map->getResourceCost(i)>0) {
+                std::cout<<"Il y a "<<gameInstance->map->getResourceCost(i)<< " " <<gameInstance->map->getResourceType(i)<< " à payer sur cette tuile." << std::endl;
+            }
+            if(gameInstance->map->getResourceType(i)=="Treasure") {
+                std::cout<<"Il y a un "<<gameInstance->map->getResourceType(i)<< " à gagner sur cette tuile." << std::endl;
+            }
+        }
+
+        for(state::Player * player : gameInstance->getPlayerList()) {
+            int i=0;
+            std::cout<< "Cales de "<< player->getName()<<" :" << std::endl;
+            for (state::BoatHold *bh:player->getBoatHolds()) {
+                std::cout<< "BoatHold N°" << i << " : " ;
+                bh->showContent();
+                i++;
+            }
+            std::cout<< "Trésors de "<< player->getName()<<" :" << std::endl;
+        }
+
+        //à compléter
+
+
+        // int playerNumber = game->getPlayerList().size();
+
+
+        // All the above is done .
+
+
+
+
+
+        // //-------------Initializes players' parameters------------
+        // const std::vector<state::Player*>& playingPlayers = game->getPlayerList();
+        // for (state::Player* player : playingPlayers) {
+        //   player->setPosition(0);
+        //   // Treasure(bonus, malus)
+        //   std::vector<state::Treasure> initialTreasures = { state::Treasure(0, 0) };
+        //   player->setTreasures(initialTreasures);
+
+        // BoatHold()
+        //définir setBoatHolds
+        // Gold() + Food()
+        //Un std::unique_ptr est un pointeur intelligent qui garantit
+        //qu'il ne peut y avoir qu'un seul propriétaire de l'objet
+        //pointé, assurant ainsi une gestion mémoire automatique et
+        //une prévention des fuites de mémoire.
+        //En utilisant std::move, cela transfère directement l’objet
+        //au lieu d'en faire une copie, ce qui est essentiel pour les
+        //unique_ptr car ils ne peuvent pas être copiés. Sans std::move,
+        //cela ne compilerait pas car un unique_ptr ne supporte pas la copie
+
+
+        //       auto goldResource = make_unique<state::Gold>();
+        //       engine::ResourceManager resource_manager;//A vire
+        //       resource_manager.addResourcesToBoathold(player,std::move(goldResource), 3,1);
+        //       auto foodResource = make_unique<state::Food>();
+        //       resource_manager.addResourcesToBoathold(player,std::move(foodResource), 3,2);
+        //       int i=1;
+        //       for (state::BoatHold *bh: player->getBoatHolds()) {
+        //         std::cout<< "BoatHold N°" << i << " : " ;
+        //         bh->showContent();
+        //         i++;
+        //       }
+
+        //       // print le type et nombre de ressource du boathold : void BoatHold::showContents()
+        //       game->displayState();
+
+        //       std::cout<<"Player " << std::to_string(player->getPlayerId()) <<" (" + player->getName() << ") initialized at position " << std::to_string(player->getPosition())<< std::endl;
+        //     }
+        //     std::cout<<"Players initialized: " << std::to_string(playingPlayers.size()) << " players in the game."<< std::endl;
+
+        return 0;
     }
-    return 0;
 }
 
-
-int Client::gameConfigInit(){
-
-    state::Game *gameInstance = gameEngine->game;
-
-    int playerNumber = inputHandler.getNumberofPlayers();
-    std::cout <<"Number of players set to: " << std::to_string(playerNumber)<< std::endl;
-    engine::ChooseNbOfPlayers* chooseNbOfPlayers = new engine::ChooseNbOfPlayers(playerNumber);
-    chooseNbOfPlayers->launchCommand(gameInstance);
-    delete chooseNbOfPlayers;
-
-    for(int playerIndex = 0; playerIndex < playerNumber; playerIndex++){
-        std::string playerName = inputHandler.getPlayerName(playerIndex);
-        engine::ChoosePlayerName* choosePlayerName = new engine::ChoosePlayerName(playerIndex, playerName);
-        choosePlayerName->launchCommand(gameInstance);
-        delete choosePlayerName;
-    }
-
-    int mapSize = inputHandler.getMapSize();
-    std::cout <<"MapSize set to: " << std::to_string(mapSize)<< std::endl;
-    engine::ChooseMapSize* chooseMapSize = new engine::ChooseMapSize(mapSize);
-    chooseMapSize->launchCommand(gameInstance);
-    delete chooseMapSize;
-
-
-    // int playerNumber = game->getPlayerList().size();
-
-
-// All the above is done .
-
-
-
-    
-    
-    // //-------------Initializes players' parameters------------
-    // const std::vector<state::Player*>& playingPlayers = game->getPlayerList();
-    // for (state::Player* player : playingPlayers) {
-    //   player->setPosition(0);
-    //   // Treasure(bonus, malus)
-    //   std::vector<state::Treasure> initialTreasures = { state::Treasure(0, 0) };
-    //   player->setTreasures(initialTreasures);
-
-      // BoatHold()
-      //définir setBoatHolds
-      // Gold() + Food()
-      //Un std::unique_ptr est un pointeur intelligent qui garantit 
-      //qu'il ne peut y avoir qu'un seul propriétaire de l'objet 
-      //pointé, assurant ainsi une gestion mémoire automatique et 
-      //une prévention des fuites de mémoire.
-      //En utilisant std::move, cela transfère directement l’objet 
-      //au lieu d'en faire une copie, ce qui est essentiel pour les 
-      //unique_ptr car ils ne peuvent pas être copiés. Sans std::move,
-      //cela ne compilerait pas car un unique_ptr ne supporte pas la copie
-      
-      
-//       auto goldResource = make_unique<state::Gold>();
-//       engine::ResourceManager resource_manager;//A vire
-//       resource_manager.addResourcesToBoathold(player,std::move(goldResource), 3,1);
-//       auto foodResource = make_unique<state::Food>();
-//       resource_manager.addResourcesToBoathold(player,std::move(foodResource), 3,2);
-//       int i=1;
-//       for (state::BoatHold *bh: player->getBoatHolds()) {
-//         std::cout<< "BoatHold N°" << i << " : " ;
-//         bh->showContent();
-//         i++;
-//       }
-
-//       // print le type et nombre de ressource du boathold : void BoatHold::showContents()
-//       game->displayState();
-
-//       std::cout<<"Player " << std::to_string(player->getPlayerId()) <<" (" + player->getName() << ") initialized at position " << std::to_string(player->getPosition())<< std::endl;
-//     }
-//     std::cout<<"Players initialized: " << std::to_string(playingPlayers.size()) << " players in the game."<< std::endl;
-
-    return 0;
-}
-
-}
