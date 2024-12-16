80ba4aa87d681a8521b718f43ff0c3a7d01b8c91
LCNRBN <lucian.robin@gmail.com>
2024-12-16 15:56:18 +0100

    stealResource command

--- src/shared/engine/StealResource.cpp
+++ src/shared/engine/StealResource.cpp
@@ -1,16 +1,18 @@
 #include "StealResource.h"
+#include "../state/BoatHold.h"
 #include "state.h"
+
 namespace engine{
-//TODO
 
-StealResource::StealResource(int boatholdIndex, int winnerIndex, int loserIndex ) : winnerIndex(winnerIndex), loserIndex(loserIndex), boatholdIndex(boatholdIndex){
-}
-
+StealResource::StealResource(int boatholdIndex, int winnerIndex, int loserIndex ) : boatholdIndex(boatholdIndex), winnerIndex(winnerIndex), loserIndex(loserIndex){}
 void const StealResource::launchCommand(state::Game * GamePtr){
     state::Player* winner = GamePtr->getPlayerList()[winnerIndex];
     state::Player* loser = GamePtr->getPlayerList()[loserIndex];
-    //winner->addResourcesToBoatHold(resourceType, boathold, quantity, 0);
-    // je le ferai plus tard
+    state::BoatHold *stolenHold = loser->getBoatHolds().at(boatholdIndex);
+    std::string resourceType = stolenHold->getResourceType();
+    int quantity = stolenHold->getQuantity();;
+    winner->addResourcesToBoatHold(resourceType, boatholdIndex, quantity, 0);
+    loser->removeFromBoatHold(boatholdIndex, quantity);
 }
 
 StealResource::~StealResource(){}
