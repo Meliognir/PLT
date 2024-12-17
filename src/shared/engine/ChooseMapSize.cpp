1a68c7282e7b680676d2717613f38a828084c835
AscentLe <35le.mathieu@gmail.com>
2024-12-17 13:55:10 +0100

    Map initialisation with render

--- src/shared/engine/ChooseMapSize.cpp
+++ src/shared/engine/ChooseMapSize.cpp
@@ -11,7 +11,6 @@ engine::ChooseMapSize::ChooseMapSize(int size) : size(size){
 }
 
 void const engine::ChooseMapSize::launchCommand (state::Game * GamePtr){
-    
     if (GamePtr->map == nullptr) {
         GamePtr->map = new state::Map(size);
     } else {
@@ -22,8 +21,9 @@ void const engine::ChooseMapSize::launchCommand (state::Game * GamePtr){
     int playerNumber = GamePtr->getPlayerList().size();
  //"Port Royal" : Start Tile : foodCost, goldCost, treasure, nbPlayer
 
-    state::Tile tile(0, "Port Royal", playerNumber);
-    GamePtr->map->listOfTiles.push_back(&tile);
+    //state::Tile tile(0, "Port Royal", playerNumber);
+    //GamePtr->map->listOfTiles.push_back(&tile);
+    GamePtr->map->listOfTiles.push_back(new state::Tile(0, "Port Royal", playerNumber));
     // other Tiles
     for (int i = 0; i < size-1; ++i) {
         int resource = TREASURE;
@@ -44,7 +44,8 @@ void const engine::ChooseMapSize::launchCommand (state::Game * GamePtr){
         GamePtr->map->listOfTiles.push_back(tile);
     }
     std::cout<<"Map initialized with " << std::to_string(GamePtr->map->getSize()) << " tiles."<< std::endl;
-
+    GamePtr->setMapReady(true);
+    std::cout <<"Map is ready"<<std::endl;
 }
 
 engine::ChooseMapSize::~ChooseMapSize(){}
