21c717be4405418abecd9e730d1cfa31e2c219a1
LCNRBN <lucian.robin@gmail.com>
2025-01-09 09:48:46 +0100

    gameoverstate

--- src/shared/state/GameOverState.cpp
+++ src/shared/state/GameOverState.cpp
@@ -22,85 +22,46 @@ namespace state {
        //quand un joueur atteint la dernière tile chaque joueur fini son tour et on compte alors les points
         for(Player * playerEnd : game->getPlayerList()) {
             int positionEnding=playerEnd->getPosition();
-            if (positionEnding <= game->map->getSize()-7) {
-                int playerScore=0;
-                playerScore-=5;
-                for (BoatHold *currentBoathold : playerEnd->getBoatHolds()) {
-                    if (currentBoathold->hasResourceType("Gold")) {
-                        playerScore+=currentBoathold->getQuantity();
-                    }
+            int bhGold = 0;
+            int playerScore = 0;
+            for(BoatHold *currentBoathold : playerEnd->getBoatHolds()){
+                if (currentBoathold->hasResourceType("Gold")) {
+                    bhGold += currentBoathold->getQuantity();
                 }
-                std::cout<< "Player : " << playerEnd->getName() << " has a score of : " << playerScore << " ."<< std::endl;
             }
-            //tile -6
-            if (positionEnding==game->map->getSize()-6) {
-                for (BoatHold *currentBoathold : playerEnd->getBoatHolds()) {
-                    if (currentBoathold->hasResourceType("Gold")) {
-                        playerScore+=currentBoathold->getQuantity();
-                    }
+            if (positionEnding <= game->map->getSize() - 7) {
+                playerScore -= 5;
+            } else {
+                int relativePosition = game->map->getSize() - positionEnding;
+
+                switch (relativePosition) {
+                    case 6:
+                        playerScore += 0;
+                        break;
+                    case 5:
+                        playerScore += 1;
+                        break;
+                    case 4:
+                        playerScore += 3;
+                        break;
+                    case 3:
+                        playerScore += 5;
+                        break;
+                    case 2:
+                        playerScore += 7;
+                        break;
+                    case 1:
+                        playerScore += 10;
+                        break;
+                    case 0:
+                        playerScore += 15;
+                        break;
+                    default:
+                        break;
                 }
-                std::cout<< "Player : " << playerEnd->getName() << " has a score of : " << playerScore << " ."<< std::endl;
             }
-            //tile -5
-            if (positionEnding==game->map->getSize()-5) {
-                playerScore+=1;
-                for (BoatHold *currentBoathold : playerEnd->getBoatHolds()) {
-                    if (currentBoathold->hasResourceType("Gold")) {
-                        playerScore+=currentBoathold->getQuantity();
-                    }
-                }
-                std::cout<< "Player : " << playerEnd->getName() << " has a score of : " << playerScore << " ."<< std::endl;
-            }
-            //tile -4
-            if (positionEnding==game->map->getSize()-4) {
-                playerScore+=3;
-                for (BoatHold *currentBoathold : playerEnd->getBoatHolds()) {
-                    if (currentBoathold->hasResourceType("Gold")) {
-                        playerScore+=currentBoathold->getQuantity();
-                    }
-                }
-                std::cout<< "Player : " << playerEnd->getName() << " has a score of : " << playerScore << " ."<< std::endl;
-            }
-            //tile -3
-            if (positionEnding==game->map->getSize()-3) {
-                playerScore+=5;
-                for (BoatHold *currentBoathold : playerEnd->getBoatHolds()) {
-                    if (currentBoathold->hasResourceType("Gold")) {
-                        playerScore+=currentBoathold->getQuantity();
-                    }
-                }
-                std::cout<< "Player : " << playerEnd->getName() << " has a score of : " << playerScore << " ."<< std::endl;
-            }
-            //tile -2
-            if (positionEnding==game->map->getSize()-2) {
-                playerScore+=7;
-                for (BoatHold *currentBoathold : playerEnd->getBoatHolds()) {
-                    if (currentBoathold->hasResourceType("Gold")) {
-                        playerScore+=currentBoathold->getQuantity();
-                    }
-                }
-                std::cout<< "Player : " << playerEnd->getName() << " has a score of : " << playerScore << " ."<< std::endl;
-            }
-            //tile -1
-            if (positionEnding==game->map->getSize()-1) {
-                playerScore+=10;
-                for (BoatHold *currentBoathold : playerEnd->getBoatHolds()) {
-                    if (currentBoathold->hasResourceType("Gold")) {
-                        playerScore+=currentBoathold->getQuantity();
-                    }
-                }
-                std::cout<< "Player : " << playerEnd->getName() << " has a score of : " << playerScore << " ."<< std::endl;
-            }
-            //tile fin
-            if (positionEnding==game->map->getSize()) {
-                playerScore+=15;
-                for (BoatHold *currentBoathold : playerEnd->getBoatHolds()) {
-                    if (currentBoathold->hasResourceType("Gold")) {
-                        playerScore+=currentBoathold->getQuantity();
-                    }
-                }
-                std::cout<< "Player : " << playerEnd->getName() << " has a score of : " << playerScore << " ."<< std::endl;
-            }
+            playerScore+=bhGold;
+            std::cout<< "Player : " << playerEnd->getName() << " has a score of : " << playerScore << " ."<< std::endl;
         }
     }
 
